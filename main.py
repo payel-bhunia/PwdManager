db72f2a690264e25b2f196ccf7d1a843dc24444a
Payel <payel.bhunia0301@gmail.com>
2021-10-25 01:25:11 +0530

    fixed4

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from app import create_app, db
 
 
-if __name__ == '__main__':
-    app = create_app()
-    with app.test_request_context():
-        db.create_all()
-    app.run(debug=True)
+#if __name__ == '__main__':
+app = create_app()
+with app.test_request_context():
+    db.create_all()
+app.run(debug=True)
 
