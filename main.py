82a10329545f8efca2d9ded4d4c93fed76b4b1f3
Payel <payel.bhunia0301@gmail.com>
2021-10-25 02:17:40 +0530

    fixed4

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from app import create_app, db
 from app.models import User
 from sqlalchemy import exc
-
+from werkzeug.security import generate_password_hash
 
 app = create_app()
 with app.test_request_context():
     db.create_all()
     try:
         if not User.query.filter_by(user_name='raai').first():
-            User.create_user(user_name='raai', user_email='raai@gmail.com', user_password='secret', user_phone_num='9903020341')
+            new_user = User(user_email='raai@gmail.com', user_name='raai', user_password=generate_password_hash(
+                'secret', method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
     except exc.IntegrityError:
         app.run(debug=True)
 
