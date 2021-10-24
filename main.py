dc1ec3cdecff08ff877103f262702dda275994c7
Payel <payel.bhunia0301@gmail.com>
2021-10-25 02:12:05 +0530

    fixed4

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from app import create_app, db
+from app.models import User
+from sqlalchemy import exc
 
 
 app = create_app()
 with app.test_request_context():
     db.create_all()
-app.run(debug=True)
+    try:
+        if not User.query.filter_by(user_name='raai').first():
+            User.create_user(user_name='raai', user_email='raai@gmail.com', user_password='secret', user_phone_num='9903020341')
+    except exc.IntegrityError:
+        app.run(debug=True)
 
