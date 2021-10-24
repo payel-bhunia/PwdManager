b81a1b9800c337089c211befc859e832d293142d
Payel <payel.bhunia0301@gmail.com>
2021-10-25 00:38:41 +0530

    fixed

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,11 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kiran@localhost:5433/pwdmng'
+    env = 'dev'
+    if env == 'dev':
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kiran@localhost:5433/pwdmng'
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vcektifmkqivgi:cd142c9f4ea504b9e8c993799ef01e31368858dd4da71f1b6976393f4ee4f157@ec2-3-209-65-193.compute-1.amazonaws.com:5432/d7cls68s9h2c1h'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.init_app(app)
     from .views import views
